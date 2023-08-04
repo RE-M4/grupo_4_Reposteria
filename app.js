026158e583d1766fb6529d56418b16e7ba717ff6
RE-M4 <evilmlr2018@gmail.com>
2023-08-03 22:18:31 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+const publicPath = path.resolve(__dirname,"./public")
+
+app.use(express.static(publicPath))
+
+app.listen(3000, () => {
+    console.log("Server is running");
+})
+
+app.get('/', function(req,res){
+    res.sendFile(path.resolve(__dirname,'./views/index.html'))
+})
