4c35ba5286c059f47edc9fbdeac6641f1ed302a5
LeoSingh-Ar <leosingh.ls11@gmail.com>
2023-10-17 10:15:50 -0300

    Sprint 5
    Actualizacion de codigo con los puntos del Sprint 5

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,73 @@
+const fs = require('fs');
 const path = require('path');
+const bcrypt = require('bcryptjs');
+
+const usersFilePath = path.join(__dirname, '../data/users.json');
+let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+
+function addUser(user) {
+    users.push(user);
+    let usersJSON = JSON.stringify(users);
+    fs.writeFileSync(usersFilePath, usersJSON);
+}
 
 const userController = {
     signUp : function(req,res){
         res.render('register')
     },
+    create: (req,res) =>{
+        
+    },
     logIn : function(req,res){
         res.render('login2')
     },
+
+    authenticate: function(req, res){
+        let formData = req.body;
+        let userFound = users.find(function(user){
+            return user.username == formData.username && bcrypt.compareSync(formData.password, user.password);
+        })
+        if(userFound){
+            req.session.user = userFound;
+            if(formData.remember){
+                res.cookie('userId', userFound.id, {maxAge: 30 * 24 * 60 * 60 * 1000});
+            }
+            res.redirect('/profile');
+        }else{
+            res.redirect('/login');
+        }
+    },
+    logout: function(req, res){
+        req.session.destroy();
+        if(req.cookies.userId){
+            res.clearCookie('userId');
+        }
+        res.redirect('/');
+    },
+
     shopCart : function(req,res){
         res.render('shopping-cart')
+    },
+    register: function(req, res){
+        res.render('register');
+    },
+    create: function(req, res){
+        let formData = req.body;
+        const newUser = {
+            id: user.length + 1,
+            first_name: formData.first_name,
+            last_name: formData.last_name,
+            username: formData.username,
+            password: bcrypt.hashSync(formData.password, 10), //Encriptacion de la contraseña
+            email: formData.email,
+            image: req.file ? req.file.filename : null //Configurar multer para manejar la subida del archivo
+        }
+        addUser(newUser);
+        res.redirect('/login');
+    },
+    profile: function(req, res){
+        res.render('profile', {user: req.session.user});
     }
 };
 
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,37 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController')
 
-router.get('/signup',userController.signUp);
-router.get('/login',userController.logIn);
+
+// Middleware para verificar si el usuario está logueado
+function authMiddleware(req, res, next) {
+    if (req.session.user) {                     // Si el usuario está logueado
+        next();                                 // Continuamos con la siguiente función 
+    } else {                                    // Si no está logueado
+        res.redirect('/login');                 // Lo redirigimos al login
+    }
+}
+
+// Middleware para verificar si el usuario es un huésped
+function guestMiddleware(req, res, next) {
+    if (!req.session.user) {                    // Si el usuario no está logueado
+        next();                                 // Continuamos con la siguiente función
+    } else {                                    // Si está logueado
+        res.redirect('/profile');               // Lo redirigimos al perfil
+    }
+}
+
+
+router.get('/signup', guestMiddleware, userController.signUp); //Solo accesible si no estas logueado
+router.get('/login', guestMiddleware, userController.logIn);
+
+// Rutas para registro, inicio de sesión y cierre de sesión
+
+router.get('/register', guestMiddleware, userController.register); // Solo accesible si no estás logueado
+router.post('/register', guestMiddleware, usersController.create); // Solo accesible si no estás logueado
+router.get('/login', guestMiddleware, usersController.login);  // Solo accesible si no estás logueado
+router.post('/login', usersController.authenticate);
+router.get('/logout', authMiddleware, usersController.logout); //Solo accesible si estas logueado
+router.get('/profile', authMiddleware, userController.profile);
+
 
 module.exports = router;
